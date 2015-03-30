962a6f36a7c2f54bac9ac3d660b2d408879457b8
Mathieu Helie <mhelie@gmail.com>
2015-03-29 21:18:21 -0400

    Fork for Infiniti

--- playlist-data.js
+++ playlist-data.js
@@ -1,145 +1,133 @@
 var playlistData = [
   {
-    "path": "Experience-Lexus-Laval",
-    "section": "Expérience",
-    "title" : "L'expérience Lexus Laval",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/Lexus2014_Sonia_Ricardo-ACCUEIL.jpg",
-    "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---1---Finale.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---1---Finale.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---1---Finale.ogv"
-	  }
-  }
-  ,
-  {
-    "path": "Temoignages",
-    "section": "Expérience",
-    "title" : "Témoignages",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/Client-philip-2-TEMOIGNAGES.jpg",
-    "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---2.1---Finale.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---2.1---Finale.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---2.1---Finale.ogv"
-    }
-  }
-  ,
-  {
-    "path": "Notre-philosophie",
-    "section": "Expérience",
-    "title" : "Notre philosophie",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/3_EQUIPE_Service.jpg",
-    "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---3.1---Finale.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---3.1---Finale.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Capsule---3.1---Finale.ogv"
-    }
-  }
-  ,
-  {
-    "path": "Introduction-des-capsules",
+    "path": "Intro",
     "section": "Produits financiers",
-    "title" : "L’expérience après-vente simplifiée",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/1_INTRO_capsule.jpg",
+    "title" : "Introduction des produits financiers",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/1_Intro-capsule.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_1_INTRODUCTION_DES_CAPSULES_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_1_INTRODUCTION_DES_CAPSULES_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_1_INTRODUCTION_DES_CAPSULES_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_1_INTRODUCTION_DES_CAPSULES_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_1_INTRODUCTION_DES_CAPSULES_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_1_INTRODUCTION_DES_CAPSULES_v4.ogv"
     }
   }
   ,
   {
-    "path": "PEA-Platine",
+    "path": "Infiniti-Platine",
     "section": "Produits financiers",
-    "title" : "La PEA Platine",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/2_LEXUS_PEA.jpg",
+    "title" : "Protection Infiniti Platine",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/2_Protection Platine.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_2_PEA_Platine_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_2_PEA_Platine_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_2_PEA_Platine_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_2_Protection_Platine_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_2_Protection_Platine_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_2_Protection_Platine_v4.ogv"
     }
   }
   ,
   {
-    "path": "Service-executif",
+    "path": "Couverture-Multiprotection",
     "section": "Produits financiers",
-    "title" : "Le service exécutif",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/3_SERVICE_executif.jpg",
+    "title" : "La couverture multiprotection",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/3_Couverture Multiprotection.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_3_Le_Service_Executif_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_3_Le_Service_Executif_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_3_Le_Service_Executif_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_3_LA_COUVERTURE_MULTIPROTECTION_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_3_LA_COUVERTURE_MULTIPROTECTION_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_3_LA_COUVERTURE_MULTIPROTECTION_v4.ogv"
     }
   }
   ,
   {
     "path": "Esthetique",
     "section": "Produits financiers",
-    "title" : "L'esthétique",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/4_LEXUS_esthetique.jpg",
+    "title" : "L'esthétique Infiniti",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/4_Esthetique-B.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_4_Esthetique_Lexus_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_4_Esthetique_Lexus_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_4_Esthetique_Lexus_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_4_Esthetique_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_4_Esthetique_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_4_Esthetique_v4.ogv"
     }
   }
   ,
   {
     "path": "Assurance-de-remplacement",
     "section": "Produits financiers",
-    "title" : "Assurance de remplacement",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/5_AssuranceRemplacement.jpg",
+    "title" : "L'assurance de remplacement",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/5_assurance de remplacement.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_5_Assurance_de_remplacement_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_5_Assurance_de_remplacement_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_5_Assurance_de_remplacement_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_5_Assurance_de__remplacement_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_5_Assurance_de__remplacement_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_5_Assurance_de__remplacement_v4.ogv"
     }
   }
   ,
   {
-    "path": "TAG",
+    "path": "Système-de-repérage",
     "section": "Produits financiers",
-    "title" : "Système de repérage TAG",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/6_LEXUS_TAG.jpg",
+    "title" : "Système de repérage",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/6_systeme de repérage.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_6_TAG_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_6_TAG_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_6_TAG_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_6_Systeme_de_reperage_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_6_Systeme_de_reperage_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_6_Systeme_de_reperage_v4.ogv"
     }
   }
   ,
   {
     "path": "Cueillette",
-    "section": "Expérience après achat",
-    "title" : "La prise de possession",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/7_LEXUS_cueillette-prise_de_possession.jpg",
+    "section": "Livraison",
+    "title" : "Cueillette du véhicule",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/7_acquisition du vehicule.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_7_Cueillette_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_7_Cueillette_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_7_Cueillette_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_7_Cueillette_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_7_Cueillette_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_7_Cueillette_v4.ogv"
     }
   }
   ,
   {
-    "path": "Personnalisation",
-    "section": "Expérience après achat",
-    "title" : "Personnalisation de votre véhicule",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/8-Personnalisation_et_suivi.jpg",
+    "path": "Retour-à-la-maison",
+    "section": "Livraison",
+    "title" : "Retour à la maison",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/8-retour à la maison .jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_8_Retour_a_la_maison_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_8_Retour_a_la_maison_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_8_Retour_a_la_maison_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_8_Retour_a_la_maison_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_8_Retour_a_la_maison_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_8_Retour_a_la_maison_v4.ogv"
+    }
+  }
+  ,
+  {
+    "path": "Programme-eLITE",
+    "section": "Livraison",
+    "title" : "Programme d'entretien prépayé élite",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/9_programme d'entretin prépayé.jpg",
+    "sources" : {
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_9_Programme_dentretien_prepaye_de_la_gamme_eLITE_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_9_Programme_dentretien_prepaye_de_la_gamme_eLITE_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_9_Programme_dentretien_prepaye_de_la_gamme_eLITE_v4.ogv"
     }
   }
   ,
   {
     "path": "Changement-de-pneus",
-    "section": "Service",
+    "section": "Livraison",
     "title" : "Changement de pneus",
-    "poster" : "http://agencewebdiffusion.github.io/lexuslaval/images/9_changementPNEU.jpg",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/10-Pneu.jpg",
     "sources" : {
-      "video/webm" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_9_Changement_de_Pneu_v7.webm",
-      "video/mp4" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_9_Changement_de_Pneu_v7.mp4",
-      "video/ogg" : "http://d2eetcfu5pmoc1.cloudfront.net/videos/v8/Lexus_9_Changement_de_Pneu_v7.ogv"
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_10_Changement_de_Pneu_v4.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_10_Changement_de_Pneu_v4.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_10_Changement_de_Pneu_v4.ogv"
+    }
+  }
+  ,
+  {
+    "path": "Véhicule-d-occasion-certifié",
+    "section": "Occasion",
+    "title" : "Véhicule d'occasion certifié Infiniti",
+    "poster" : "http://agencewebdiffusion.github.io/infinitilaval/images/11_b-vehicule certifié.jpg",
+    "sources" : {
+      "video/webm" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_11_Vehicule_d_occasion_certifie_v4_grfx.webm",
+      "video/mp4" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_11_Vehicule_d_occasion_certifie_v4_grfx.mp4",
+      "video/ogg" : "http://d3fa3ms7js8ulf.cloudfront.net/videos/WEBv01/Infiniti_11_Vehicule_d_occasion_certifie_v4_grfx.ogv"
     }
   }
 ];
